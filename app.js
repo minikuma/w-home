f8f7c278d6566e86760f43c4f9f84a3bcea1b049
minikuma <minikuma@naver.com>
2020-10-15 01:05:37 +0900

    ver.0.0.2 new modify

--- app.js
+++ app.js
@@ -4,25 +4,143 @@
  * Github : http://github.com/minikuma
  */
 
-const express = require('express');
+'use strict';
+
 const fs = require('fs');
-const serveStatic = require('serve-static');
+const multer = require('multer');
+const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const routes = require('./routes/uploadRoute');
+const router = express.Router();
 const app = express();
 
-app.set('port', 4000);
-app.use(express.static(__dirname + '/public')); // html 적용
-app.use('uploads', serveStatic(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
+app.use('/', router);
 app.use(cors());
-app.use('/', routes); // 라우터 기본 경로 등록
 
-app.all('*', function (req, res) {
-    res.status(404).send('<h1> 요청 페이지 없음 </h1>')
+const storage = multer.diskStorage({
+   destination(req, file, callback) {
+       callback(null, 'uploads');
+   },
+   filename(req, file, callback) {
+       let array = file.originalname.split('.');
+       array[0] = array[0] + '_';
+       array[1] = '.' + array[1];
+       array.splice(1, 0, Date.now().toString());
+       const result = array.join('');
+       callback(null, result);
+   }
+});
+
+const upload = multer({
+   storage,
+   limits: {
+       files: 10,
+       fileSize: 1024 * 1024 * 1024,
+   }
+});
+
+// 업로드 라우터
+router.post('/upload', upload.array('print', 1), function (req, res, next) {
+    try {
+        const files = req.files;
+        let originalName = '';
+        let fileName = '';
+        let mimeType = '';
+        let size = 0;
+
+        if (Array.isArray(files)) {
+            console.log('files....');
+            originalName = files[0].originalname;
+            fileName = files[0].filename;
+            mimeType = files[0].mimeType;
+            size = files[0].size;
+        } else {
+            console.log('not files....');
+            originalName = files[0].originalname;
+            fileName = files[0].filename;
+            mimeType = files[0].mimeType;
+            size = files[0].size;
+        }
+
+        console.log(`file: ${originalName}, ${fileName}, ${mimeType}, ${size}`);
+
+        res.writeHead(200, {
+           'Content-type': 'text/html;charset=utf8'
+        });
+
+        res.write('<h3>upload success</h3>');
+        res.write(`<p>original name = ${originalName}, saved name = ${fileName}</p>`);
+        res.write(`<p>mime type = ${mimeType}</p>`);
+        res.write(`<p>size = ${size}</p>`);
+        res.end();
+    } catch (error) {
+        console.dir(error.stack);
+    }
+});
+
+// 리스트
+router.get('/', function (req, res, next) {
+
+    console.log(`params: ${req.query.id}`);
+
+    if (req.query.id == undefined) {
+        fs.readdir('./uploads', function (err, filelist) {
+            let title = 'Welcome! Print World!';
+            let description = 'Hello, Print';
+            let list = '<ul>';
+            let i = 0
+            while (i < filelist.length) {
+                list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a> </li>`;
+                i = i + 1;
+            }
+            list = list + '</ul>'
+            // template
+            let template = `
+                    <!doctype html>
+                    <html>
+                        <head>
+                            <title>WEB1 - ${title}</title>
+                            <meta charset="UTF-8">
+                        </head>
+                        <body>
+                            <h1><a href="/">WEB</a></h1>
+                            ${list}
+                            <h2>${title}</h2>
+                            <p>${description}</p>
+                        </body>
+                    </html>
+                `;
+
+            res.writeHead(200);
+            res.end(template);
+        });
+    } else {
+        console.log('downloading....');
+        fs.readdir('./uploads', function (err, filelist) {
+            let title = 'Welcome! Print World!';
+            let description = 'Hello, Print';
+            let list = '<ul>';
+            let i = 0
+            while (i < filelist.length) {
+                list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a> </li>`;
+                i = i + 1;
+            }
+            list = list + '</ul>'
+
+            // 다운로드
+            const uploadFolder = './uploads/';
+            const file = uploadFolder + req.query.id;
+            const filename = path.basename(file);
+            res.setHeader('Content-disposition', 'attachment: filename=' + filename);
+            res.setHeader('Content-type', "application/octet-stream");
+            const filestream = fs.createReadStream(file);
+            filestream.pipe(res);
+        });
+    }
 });
 
 app.listen(4000, () => {
-    console.log("Node js Express Server Start!");
+    console.log('Ready to Server....');
 });
-
